a2e6ba655d33d9832dc4e4a2d871c04f943a0569
Hinajawaid <97847721+Hinajawaid@users.noreply.github.com>
2024-11-22 11:13:39 +0500

    Add files via upload

---
+++ javaFile.java
@@ -0,0 +1,9 @@
+public class javaFile {
+    public static void main(String[] args) {
+        // Define the string
+        String studentId = "FA21-BCS-027";
+
+        // Print the string to the console
+        System.out.println(studentId);
+    }
+}
